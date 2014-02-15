32ff63fd55ab6783a7bec6a3aef128ed65d537c9
Ivan Sugerman <ivan@ivansugerman.com>
2014-02-14 22:33:26 -0500

    changes

--- src/js/soc.js
+++ src/js/soc.js
@@ -1,126 +1,111 @@
 (function (window) {
 	"use strict";
 
-	var services = {
-	    facebook: { identifier: "facebook", title:"Facebook", color:"#3B5998"},
-	    twitter: { identifier: "twitter", title:"Twitter", color:"#55ACEE"},
-	    pinterest: { identifier: "pinterest", title:"Pinterest", color:"#CB2027"},
-	   	google: { identifier: "google", title:"Google Plus", color:"#DD4B39"},
-	    youtube: { identifier: "youtube", title:"You Tube", color:"#BB0000"},
-	    linkedin: { identifier: "linkedin", title:"LinkedIn", color:"#007BB6"},
-	    instagram: { identifier: "instagram", title:"Instagram", color:"#517FA4"},
-	    yelp: { identifier: "yelp", title:"Yelp", color:"#C83218"}
-  	};
-
-	var defaults = {
-	    icons: [],
-	    size: 40,
-	    radius: "auto",
-	    color: "auto",
-	    stylesheet: "//socjs.com/soc.min.css"
+	//defauls for icons
+	var settings = {
+	    size: "30px", //width in pixels
+	    radius: "auto", //border radius in px, auto will be proportional to size
+	    spacing: "10px", //spacing between icons
+	    iconcolor: "#fff", 
+	    buttoncolor: "auto"
 	};
 
-	//utility function to extend config with defaults
-	function extend(a, b) {
-	    for( var i in b )
-	    	a[i] = b[i];
-	    return a;
-	}	
+	//merge data atributes with defaults
+	function loadDataAttr(el){
+
+		for (var key in settings) {
+			if(el.getAttribute('data-'+key))
+				settings[key]=el.getAttribute('data-'+key);
+		}
+	}
+
+	function render(icons){
+		for (var i = 0; i < icons.length; i++) {
+			setStyle(icons[i]);
+		}
+
+		//set class of last icon
+		icons[icons.length-1].className += " soc-icon-last";
+	}
 
 	//set style of an icon
-	function setStyle(el,settings,iconDefaults){
+	function setStyle(el){
+		var sizeNum = parseInt( settings.size.replace("px","") );
 
 		// the auto radius is 15% of the size of the icon
 		if(settings.radius=='auto')
-			settings.radius = settings.size*.15;
+			settings.radius = sizeNum*.15+"px";
 
+		// 50% of the button works well for a circle
 		if(settings.radius=='circle')
-			settings.radius = settings.size*.5;
+			settings.radius = sizeNum*.5+"px";
 
-		el.style.borderRadius = settings.radius+"px";
-		el.style.MozBorderRadius = settings.radius+"px";
-		el.style.WebkitBorderRadius = settings.radius+"px";
-		el.style.width = settings.size+"px";
-		el.style.height = settings.size+"px";
-		el.style.fontSize = (settings.size*.55)+"px";
-		el.style.lineHeight = settings.size+"px";
-		el.style.backgroundColor = settings.color;
+		el.style.borderRadius = settings.radius;
+		el.style.MozBorderRadius = settings.radius;
+		el.style.WebkitBorderRadius = settings.radius;
+		el.style.width = settings.size;
+		el.style.height = settings.size;
+		el.style.fontSize = (sizeNum*.55)+"px";
+		el.style.lineHeight = settings.size;
+		el.style.marginRight = settings.spacing;
 
-		if(settings.color=='auto')
-			el.style.backgroundColor = iconDefaults.color;
+		if(settings.buttoncolor != 'auto')
+			el.style.backgroundColor = settings.buttoncolor;
 
+		el.style.color = settings.iconcolor;
 	}
 
-	function loadDefaultStyle(stylesheet){
-		var fileref=document.createElement("link")
-  		fileref.setAttribute("rel", "stylesheet")
-  		fileref.setAttribute("type", "text/css")
-  		fileref.setAttribute("href", stylesheet);
-  		if (typeof fileref!="undefined")
-  			document.getElementsByTagName("head")[0].appendChild(fileref)
+	//simple function to check ie version. I can't believe we need icon. 
+	function getIEVersion()
+	{
+	  var rv = -1; // Return value assumes failure.
+	  if (navigator.appName == 'Microsoft Internet Explorer')
+	  {
+	    var ua = navigator.userAgent;
+	    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+	    if (re.exec(ua) != null)
+	      rv = parseFloat( RegExp.$1 );
+	  }
+	  return rv;
 	}
 
-	function createIcon(icon,settings){
-
-		//create icon
-		var iconEl = document.createElement('a');
-		iconEl.className = "si si-"+icon.identifier;
-		iconEl.href = settings.icons[icon.identifier].link;
-		iconEl.target = "_blank";
-
-		//add text for screen readers
-		var iconText = document.createElement('span');
-		iconText.style.display = "block";
-		iconText.style.position = "absolute";
-		iconText.style.left = "-9999px";
-		iconText.innerHTML = icon.title;
-		iconEl.appendChild(iconText);
-
-		//set the icon style
-		setStyle(iconEl,settings,icon);
-
-		//append icon to list element
-		//var iconItem = document.createElement('li');
-		//iconItem.appendChild(iconEl);
-
-		return iconEl;
+	function initIE(){
+		//:before and icon fonts don't play well in ie8 so we need to force redraw the :before elements after the font loads. Ick.
+		var head = document.getElementsByTagName('head')[0];
+		var style = document.createElement('style');
+		style.type = 'text/css';
+		style.styleSheet.cssText = ':before{content:none !important';
+		head.appendChild(style);
+		setTimeout(function(){
+			head.removeChild(style);
+		}, 0);
 	}
 
 	// social plugin constructor
-	function Social( options ) {
-		this.settings = extend( defaults, options );
+	function Soc() {
 		this.init();
 	}
 
-	Social.prototype = {
+	Soc.prototype = {
 		init: function () {
-			loadDefaultStyle(this.settings.stylesheet);
+			window.onload = function() {
+				this.el = document.getElementById('soc');
+				this.icons = this.el.getElementsByTagName('a');
+				loadDataAttr(this.el);
+
+				if(getIEVersion()=='8')
+					initIE();
+				
+				render(this.icons);
+			}
 		},
-		render: function(el){
-			var _this = this;
-
-			//make element available
-			_this.container = document.createElement('div');
-
-			//create icon list container
-			_this.container.className = "si-horizontal si-container";
-
-			//append icons to list
-			for (var key in _this.settings.icons) {
-				var icon = createIcon( services[key], _this.settings);
-				_this.container.appendChild( icon );
-			};
-
-			//add class to last icon for formatting
-			_this.container.lastChild.className = "si-last";
-			
-			//append icons to provided element
-			el.appendChild(_this.container);
-		},
-		destroy: function(){
-			this.container.parentNode.removeChild(this.container);
+		update: function(){
+			loadDataAttr(this.el);
+			render(this.icons);
 		}
 	};
 
-	window.Social = Social;
+	window.Soc = Soc;
 }(window));
+
+var soc = new Soc();
